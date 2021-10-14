3352394a706894afd5c44dc5c940a99b6f2a0c16
Max Santos <max@zippclub.com>
2021-10-14 00:31:31 -0300

    refactoring tests

--- backend/tests/Feature/Exchange/SimulateExchangeCurrencyTest.php
+++ backend/tests/Feature/Exchange/SimulateExchangeCurrencyTest.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use App\Enums\CurrencyMessages;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Bus;
 
 // models
 use App\Models\{
@@ -15,32 +16,62 @@ use App\Models\{
     User
 };
 
+// jobs 
+use App\Jobs\SendExchangeCurrencyEmail;
+
 class SimulateExchangeCurrencyTest extends TestCase {
 
     use DatabaseMigrations;
     
-    public function testSimulateBuyWithCreditCard() {
+    public function testSimulateBuyWithBoleto() {
 
         $user = User::factory()->create();
         $payment_method = PaymentMethod::where('name', 'boleto')->first();
 
-        $fake_response = json_decode(file_get_contents('tests/Responses/response_currency_usd_brl.json'), true);
-
-        Http::fake([
-            env("AWESOME_API")."last/BRL-USD" => Http::response(
-                $fake_response,
-                200
-            )
-        ]);
+        $this->simulateEndpointRequest();
+        $source_currency_value = rand(1000, 100000);
 
         $response = $this->actingAs($user)
                         ->postJson('/api/exchange/simulate', [
                             'source_currency_code'      => 'BRL',
                             'destination_currency_code' => 'USD',
-                            'source_currency_value'     => 5000,
+                            'source_currency_value'     => $source_currency_value,
                             'payment_method_id'         => $payment_method->id
                         ]);
 
+        $this->assertResponseJson($response);
+
+        $response = json_decode($response->getContent())->data;
+
+        $this->assertReturnValues($response, $source_currency_value, $payment_method);
+
+    }
+    
+    public function testSimulateBuyWithCreditCard() {
+
+        $user = User::factory()->create();
+        $payment_method = PaymentMethod::where('name', 'cartao_de_credito')->first();
+
+        $this->simulateEndpointRequest();
+        $source_currency_value = rand(1000, 100000);
+
+        $response = $this->actingAs($user)
+                        ->postJson('/api/exchange/simulate', [
+                            'source_currency_code'      => 'BRL',
+                            'destination_currency_code' => 'USD',
+                            'source_currency_value'     => $source_currency_value,
+                            'payment_method_id'         => $payment_method->id
+                        ]);
+
+        $this->assertResponseJson($response);
+
+        $response = json_decode($response->getContent())->data;
+
+        $this->assertReturnValues($response, $source_currency_value, $payment_method);
+
+    }
+
+    private function assertResponseJson(\Illuminate\Testing\TestResponse $response) {
         $response->assertStatus(200)
             ->assertJsonStructure([
                 'success',
@@ -57,34 +88,46 @@ class SimulateExchangeCurrencyTest extends TestCase {
                 ]
             ]);
 
-        $response = json_decode($response->getContent())->data;
+        Bus::assertDispatched(SendExchangeCurrencyEmail::class);
+    }
 
-        \Log::info(json_encode($response));
+    private function assertReturnValues(Object $response, Float $source_currency_value, PaymentMethod $payment_method) {
 
-        // $assert_destination_currency_bid_value = round($fake_response['USDBRL']['bid'], 2);
-        // $assert_payment_method_tax_value = $this->calculatePaymentMethodTaxValue(5000, $payment_method);
-        // $assert_exchange_tax_value = $this->calculateExchangeTaxValue(5000);
-        // $assert_exchange_used_value = $this->calculateValueAfterTaxes(5000, $assert_payment_method_tax_value, $assert_exchange_tax_value);
-        // $total_after_taxes = $this->calculateValueAfterTaxes(5000, $assert_payment_method_tax_value, $assert_exchange_tax_value);
-        // $assert_destination_currency_total_bough_value = $this->calculateValueInDestinationCurrency($total_after_taxes, $assert_destination_currency_bid_value);
+        $bid_value = $this->getBidValue();
 
-        // $this->assertEquals($response->source_currency_code, 'BRL');
-        // $this->assertEquals($response->destination_currency_code, 'USD');
-        // $this->assertEquals($response->source_currency_value, 5000);
-        // $this->assertEquals($response->destination_currency_bid_value, $assert_destination_currency_bid_value);
-        // $this->assertEquals($response->destination_currency_total_bough_value, $assert_destination_currency_total_bough_value);
-        // $this->assertEquals($response->payment_method_tax_value, $assert_payment_method_tax_value);
-        // $this->assertEquals($response->exchange_tax_value, $assert_exchange_tax_value);
-        // $this->assertEquals($response->exchange_used_value, $assert_exchange_used_value);
+        $assert_exchange_tax_value = $this->calculateExchangeTaxValue($source_currency_value);
+        $assert_payment_method_tax_value = $this->calculatePaymentMethodTaxValue($source_currency_value, $payment_method);
+        $assert_exchange_used_value = $this->calculateValueAfterTaxes($source_currency_value, $assert_payment_method_tax_value, $assert_exchange_tax_value);
+        $assert_destination_currency_total_bough_value = $this->calculateValueInDestinationCurrency($assert_exchange_used_value, $bid_value);
         
         $this->assertEquals($response->source_currency_code, 'BRL');
         $this->assertEquals($response->destination_currency_code, 'USD');
-        $this->assertEquals($response->source_currency_value, 5000);
-        $this->assertEquals($response->destination_currency_bid_value, 5.51);
-        $this->assertEquals($response->destination_currency_total_bough_value, 885.21);
-        $this->assertEquals($response->payment_method_tax_value, 72.5);
-        $this->assertEquals($response->exchange_tax_value, 50);
-        $this->assertEquals($response->exchange_used_value, 4877.5);
+        $this->assertEquals($response->source_currency_value, $source_currency_value);
+        $this->assertEquals($response->destination_currency_bid_value, $bid_value);
+        $this->assertEquals($response->destination_currency_total_bough_value, $assert_destination_currency_total_bough_value);
+        $this->assertEquals($response->payment_method_tax_value, $assert_payment_method_tax_value);
+        $this->assertEquals($response->exchange_tax_value, $assert_exchange_tax_value);
+        $this->assertEquals($response->exchange_used_value, $assert_exchange_used_value);
+    }
+
+    private function getBidValue() {
+        $fake_response = json_decode(file_get_contents('tests/Responses/response_currency_usd_brl.json'), true);
+
+        return round($fake_response['USDBRL']['bid'], 2);
+    }
+
+    private function simulateEndpointRequest() {
+
+        $fake_response = json_decode(file_get_contents('tests/Responses/response_currency_usd_brl.json'), true);
+
+        Http::fake([
+            env("AWESOME_API")."last/BRL-USD" => Http::response(
+                $fake_response,
+                200
+            )
+        ]);
+
+        return $fake_response;
 
     }
 
